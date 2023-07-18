36ad982355c7544671f5a878d700ee1e5ad435b4
Tom Freed <tom.freed@outlook.com>
2023-07-18 12:51:01 +0100

    update

--- qa.py
+++ qa.py
@@ -1,11 +1,3 @@
-word1 = "Good"
-word2 = "Day"
-word3 = "Tom"
-
-sent = word1 + " " + word2 + " " + word3 + " "
-
-print(sent)
-
 books = {"Margaret Atwood":"The Handmaiden's Tale", "J.R.R. Tolkien":["The Hobbit", "The Lord of the Rings"], "Roald Dahl":"Charlie and the Chocolate Factory"}
 author_input = input("Enter author: ")
 author_list = books[author_input]
