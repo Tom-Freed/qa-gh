5f12726fbb672dbb08dc6b7b2286f9ec4589500b
Tom Freed <tom.freed@outlook.com>
2023-07-19 12:39:06 +0100

    Adding folder

---
+++ qaex-modules/dice.py
@@ -0,0 +1,4 @@
+import random
+def random1to6():
+    num = random.randint(1,6)
+    return num
---
+++ qaex-modules/throw.py
@@ -0,0 +1,6 @@
+from dice import random1to6
+
+dice1 = random1to6()
+dice2 = random1to6()
+
+print(dice1,",",dice2)
