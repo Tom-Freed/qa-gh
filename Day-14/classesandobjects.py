229b130a9cfc71136e413b38c87c76edf3903c6f
Tom Freed <tom.freed@outlook.com>
2023-07-24 11:31:51 +0100

    update

--- Day-14/classesandobjects.py
+++ Day-14/classesandobjects.py
@@ -1,16 +1,24 @@
 #Exercise
 class Student:
-
-    def __init__(self, name, age, stu_class="student"):
+    '''
+    Creates Student class with init and avg_test functions
+    '''
+    def __init__(self, name: str, age: str, stu_class: str="student") -> None:
+        '''
+        Initalises the object setting the parameters
+        '''
         self.name = name
         self.age = age
         self.stu_class = stu_class
 
-    def avg_test(self,mark1, mark2, mark3):
-        print((mark1 + mark2 + mark3)/3)
+    def avg_test(self, mark1: int, mark2: int, mark3: int) -> float:
+        '''
+        Returns a float of the avg of the 3 tests
+        '''
+        return (mark1 + mark2 + mark3)/3
 
-Bob = Student('Bob', '25')
 
-print(Bob.age, Bob.stu_class)
-
-Bob.avg_test(60, 90, 27)
+if __name__ == '__main__':
+    Bob = Student('Bob', '25')
+    print(Bob.name, Bob.age, Bob.stu_class)
+    print(f"{Bob.name}'s average test result {Bob.avg_test(60, 90, 30)}")
