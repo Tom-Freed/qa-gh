767154b6d3f831c4062fa0fc13f088755b6c4528
Tom Freed <tom.freed@outlook.com>
2023-08-02 16:41:04 +0100

    TF: adding folder

---
+++ Day-23/getting-started.js
@@ -0,0 +1,11 @@
+console.log('Tom');
+console.info('Freed');
+console.warn('Bedford');
+console.error('Pisces');
+
+let make = "Aston Martin"
+let model = 'DB5'
+
+console.log(`My favourite car is the ${make} ${model}`)
+
+console.log('%c Hello World', "color: orange; font-family:fantasy; font-style: Bold; background-color: black; padding: 10px")
