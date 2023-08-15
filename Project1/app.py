115ae6597d01136f56bec563bdd5738d653aa04a
Tom Freed <tom.freed@outlook.com>
2023-08-15 12:34:01 +0100

    add to basket

--- Project1/app.py
+++ Project1/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import SelectField, SubmitField, IntegerField
 #pymysql
 import os
 
@@ -8,6 +10,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.sqlite"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 
 #"mysql+pymysql://root:pass@host/plant_stock"
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
@@ -20,31 +23,62 @@ class Stock(db.Model):
     name = db.Column(db.String(30), nullable=False)
     price = db.Column(db.Float, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
-    order = db.relationship('Order', backref='orderbr')
+    summary = db.Column(db.String, nullable=False) 
+    basket = db.relationship('Basket', backref='basketbr')
 
-class Order(db.Model):
+class Basket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    quantity = db.Column(db.Integer, nullable=False)
     stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'),nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total = db.Column(db.Float, nullable=False)
 
-# May need an association table
+class QuantityForm(FlaskForm):
+    stock_id = IntegerField('Stock ID')
+    quantity = SelectField("Quantity", choices=[(1),(2),(3),(4),(5)])
+    submit = SubmitField("Add to basket")
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/products')
+@app.route('/products', methods=["GET", "POST"])
 def products():
     stock = Stock.query.all()
-    return render_template('products.html', stock=stock)
+    form = QuantityForm()
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            quantity = form.quantity.data
+            stock_id = form.stock_id.data
+            form.quantity.data = 1
+            add_to_basket(stock_id, quantity)
+
+    return render_template('products.html', stock=stock, form=form)
+
+def add_to_basket(plant_id, qty):
+    with app.app_context():
+        stock = db.session.get(Stock, plant_id)
+        if stock:
+            basket_item = Basket(
+                stock_id = plant_id, 
+                price = stock.price,
+                quantity = qty,
+                total = (stock.price * int(qty))
+        )
+        db.session.add(basket_item)
+        db.session.commit()
+
 
 @app.route('/categories')
 def categories():
     return render_template('categories.html')
 
-@app.route('/cart')
-def cart():
-    return render_template('cart.html')
+@app.route('/basket')
+def basket():
+    basket = Basket.query.all()
+    return render_template('basket.html', basket=basket)
 
 @app.route('/checkout')
 def checkout():
--- Project1/create.py
+++ Project1/create.py
@@ -3,12 +3,41 @@ from app import db, app, Stock
 with app.app_context():
     db.drop_all()
     db.create_all()
-    plant1 = Stock(name='Plant1', price=10.00, stock=10)
-    plant2 = Stock(name='Plant2', price=11.25, stock=10)
-    plant3 = Stock(name='Plant3', price=12.50, stock=10)
-    plant4 = Stock(name='Plant4', price=13.75, stock=10)
-    plant5 = Stock(name='Plant5', price=14.00, stock=10)
-    db.session.add_all([plant1, plant2, plant3, plant4, plant5])
+    plant1 = Stock(
+        name='Plant1', 
+        price=10.00, 
+        stock=10,
+        summary = 'Plant 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac varius nisi. Aliquam vitae diam tincidunt, porttitor erat lobortis, tincidunt felis. Aenean ultricies ligula nec neque interdum congue. Maecenas quis laoreet ligula. Mauris ornare laoreet erat eget porta. Donec gravida dui sit amet risus semper, vel pellentesque libero rhoncus. Fusce.'
+    )
+    plant2 = Stock(
+        name='Plant2',
+        price=11.25, 
+        stock=10,
+        summary = 'Plant 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac varius nisi. Aliquam vitae diam tincidunt, porttitor erat lobortis, tincidunt felis. Aenean ultricies ligula nec neque interdum congue. Maecenas quis laoreet ligula. Mauris ornare laoreet erat eget porta. Donec gravida dui sit amet risus semper, vel pellentesque libero rhoncus. Fusce.'
+    )
+    plant3 = Stock(
+        name='Plant3', 
+        price=12.50, 
+        stock=10,
+        summary = 'Plant 3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac varius nisi. Aliquam vitae diam tincidunt, porttitor erat lobortis, tincidunt felis. Aenean ultricies ligula nec neque interdum congue. Maecenas quis laoreet ligula. Mauris ornare laoreet erat eget porta. Donec gravida dui sit amet risus semper, vel pellentesque libero rhoncus. Fusce.'
+    )
+    plant4 = Stock(
+        name='Plant4', 
+        price=13.75, 
+        stock=10,
+        summary = 'Plant 4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac varius nisi. Aliquam vitae diam tincidunt, porttitor erat lobortis, tincidunt felis. Aenean ultricies ligula nec neque interdum congue. Maecenas quis laoreet ligula. Mauris ornare laoreet erat eget porta. Donec gravida dui sit amet risus semper, vel pellentesque libero rhoncus. Fusce.'
+    )
+    plant5 = Stock(
+        name='Plant5', 
+        price=14.00, 
+        stock=10,
+        summary = 'Plant 5 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac varius nisi. Aliquam vitae diam tincidunt, porttitor erat lobortis, tincidunt felis. Aenean ultricies ligula nec neque interdum congue. Maecenas quis laoreet ligula. Mauris ornare laoreet erat eget porta. Donec gravida dui sit amet risus semper, vel pellentesque libero rhoncus. Fusce.'
+    )
+    plant6 = Stock(
+        name='Plant6',
+        price=14.25,
+        stock=10,
+        summary = 'Plant 6 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac varius nisi. Aliquam vitae diam tincidunt, porttitor erat lobortis, tincidunt felis. Aenean ultricies ligula nec neque interdum congue. Maecenas quis laoreet ligula. Mauris ornare laoreet erat eget porta. Donec gravida dui sit amet risus semper, vel pellentesque libero rhoncus. Fusce.'
+    )
+    db.session.add_all([plant1, plant2, plant3, plant4, plant5, plant6])
     db.session.commit()
-    plant1_remaining = Stock.query.filter_by(name='Plant1').first()
-    print(plant1_remaining.stock)
